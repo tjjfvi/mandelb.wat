49367dbe6d220084b2a56be4be973bdc510f1817
tjjfvi <t6@t6.fyi>
2023-01-29 08:26:03 -0500

    configure deno deploy

--- server.ts
+++ server.ts
@@ -1,7 +1,9 @@
 import { serve } from "https://deno.land/std@0.167.0/http/server.ts";
 import { serveDir } from "https://deno.land/std@0.167.0/http/file_server.ts";
 
-await Deno.run({ cmd: ["deno", "task", "build"] }).status();
+if (Deno.env.get("DEV")) {
+  await Deno.run({ cmd: ["deno", "task", "build"] }).status();
+}
 
 serve(async (req) => {
   const res = await serveDir(req, {
